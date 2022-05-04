ad697a404f4d5dd658b7ab3a2e300c20969f919e
abihedia <51483423+abihedia@users.noreply.github.com>
2022-05-04 17:25:35 +0200

    Add files via upload

--- Custom_fields/model/partner_model_inherit.py
+++ Custom_fields/model/partner_model_inherit.py
@@ -7,3 +7,15 @@ class PartnerModelHerit(models.Model):
     parc_machine = fields.One2many('fleet.vehicle', 'partner_id')
     num_siren = fields.Char('N° SIREN')
     activity = fields.Char('activité')
+    origine = fields.Many2one('partner.origin', string='Origine')
+    montant_tot_partenariat = fields.Float('Montant total du partenariat')
+    montant_rest_regl = fields.Float('Montant restant à régler')
+    factures_partner = fields.One2many('account.move', 'partner_id')
+
+
+class OriginPartner(models.Model):
+    _name = 'partner.origin'
+    _description ='Origin'
+
+    name = fields.Char('Nom')
+
