5d1ea28daf6783e373ee8bdbd7699cd76a015541
abihedia <51483423+abihedia@users.noreply.github.com>
2022-05-08 22:58:04 +0200

    Add files via upload

--- Custom_fields/model/partner_model_inherit.py
+++ Custom_fields/model/partner_model_inherit.py
@@ -6,12 +6,19 @@ class PartnerModelHerit(models.Model):
     type_contact = fields.Selection([('Prospect', 'Prospect'), ('Client', 'Client')])
     parc_machine = fields.One2many('fleet.vehicle', 'partner_id')
     num_siren = fields.Char('N° SIREN')
-    activity = fields.Char('Activité')
+    activity = fields.Many2one('mail.activity', String="Activité")
     origine = fields.Many2one('partner.origin', string='Origine')
     montant_tot_partenariat = fields.Float('Montant total du partenariat')
-    montant_rest_regl = fields.Float('Montant restant à régler')
+    montant_rest_regl = fields.Float('Montant restant à régler', compute='_compute_amount_partner')
     factures_partner = fields.One2many('account.move', 'partner_id')
 
+    @api.depends('factures_partner.amount_residual_signed')
+    def _compute_amount_partner(self):
+        amount = 0.0
+        for par in self:
+            for rec in par.factures_partner:
+                amount += rec.amount_residual_signed
+            par.montant_rest_regl = par.montant_tot_partenariat - amount
 
 class OriginPartner(models.Model):
     _name = 'partner.origin'
