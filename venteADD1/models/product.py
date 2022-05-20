9296d0549ed25f773db795e905a2f253c4a65faf
abihedia <51483423+abihedia@users.noreply.github.com>
2022-05-20 11:12:59 +0100

    Delete venteADD1 directory

--- venteADD1/models/product.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, fields, models
-
-
-class ProductProductInherit(models.Model):
-    _inherit = "product.template"
-    parc_ok = fields.Boolean(default=False, string="Peut être un Parc")
-    serie_number_materiel = fields.Char(string="N° serie")
