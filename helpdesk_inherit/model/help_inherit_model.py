48c29d2adff51b50a14b9f419202ac7184e39d02
abihedia <51483423+abihedia@users.noreply.github.com>
2022-05-24 16:09:01 +0200

    Add files via upload

--- helpdesk_inherit/model/help_inherit_model.py
+++ helpdesk_inherit/model/help_inherit_model.py
@@ -5,5 +5,5 @@ class HelpdeskModelHerit(models.Model):
     _inherit = 'helpdesk.ticket'
 
     products_id = fields.Many2one('product.product', string='Article')
-    lots_id = fields.Many2one('stock.production.lot', string='Lot/numéro de série')
-
+    lots_id = fields.Many2one('stock.production.lot', string='Lot/numéro de série', help="Lot/Serial number concerned by the ticket", domain="[('product_id', '=', products_id)]")
+    tracking = fields.Selection(related='products_id.tracking')
