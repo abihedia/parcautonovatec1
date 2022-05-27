3c1da6a1d715899fe203e7d6fac8e570ef5f3d57
abihedia <51483423+abihedia@users.noreply.github.com>
2022-05-27 10:18:42 +0100

    Update help_inherit_model.py

--- helpdesk_inherit/model/help_inherit_model.py
+++ helpdesk_inherit/model/help_inherit_model.py
@@ -5,5 +5,5 @@ class HelpdeskModelHerit(models.Model):
     _inherit = 'helpdesk.ticket'
 
     products_id = fields.Many2one('fleet.vehicle', string='Article', default=lambda self: self.env['fleet.vehicle'].search([('partner_id', '=', self.partner_id.id)]))
-    lots_id = fields.Many2one('stock.production.lot', string='Lot/numéro de série', help="Lot/Serial number", domain="[('product_id', '=', products_id)]")
+    lots_id = fields.Many2one('stock.production.lot', string='Lot/numéro de série', help="Lot/Serial number")
     tracking = fields.Selection(related='products_id.tracking')
