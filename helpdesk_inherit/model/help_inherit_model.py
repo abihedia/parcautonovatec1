d9d6911cf3684075cf83c98b4dbd5934fc8273d5
abihedia <51483423+abihedia@users.noreply.github.com>
2022-05-31 10:18:00 +0100

    Update help_inherit_model.py

--- helpdesk_inherit/model/help_inherit_model.py
+++ helpdesk_inherit/model/help_inherit_model.py
@@ -6,6 +6,15 @@ class HelpdeskModelHerit(models.Model):
 
     products_id = fields.Many2one('product.product', string='Article', default=lambda self: self.env['fleet.vehicle'].search([('partner_id', '=', self.partner_id.id)]).fleet_artic_id)
     lots_id = fields.Many2one('stock.production.lot', string='Lot/numéro de série', help="Lot/Serial number", domain="[('product_id', '=', products_id)]")
+    article_id = fields.Many2one('product.product', string='Article')
+    
+    
+    
+    @api.onchange('partner_id')
+    def onchange_partner_id(self):
+        partner_fleet = self.env['fleet.vehicle'].search([('partner_id', '=', self.partner_id.id)])
+        for rec in partner_fleet:
+            return {'domain': {'article_id' : [(self.partner_fleet.partner_id, '=', self.partner_id.id)]}}
 
             
                 
